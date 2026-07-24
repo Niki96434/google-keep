3e17fa55df25b87409da4a3f60a5640f4cf218fb
nikki <cozercaniegirl143@mail.ru>
2026-07-24 20:57:49 +0300

    chore(deps): add prettierrc and eslint formatters

---
+++ apps/backend/src/core/server.ts
@@ -0,0 +1,10 @@
+import type { Request, Response } from 'express'
+import { server } from './app'
+
+const port = Number(process.env.PORT) || 3000
+
+server.get('/', (_req: Request, res: Response) => {
+  res.send('hello')
+})
+
+server.listen(port)
