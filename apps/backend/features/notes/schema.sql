ecd59dda0b1514487224f003e88c4e570697822d
nikki <cozercaniegirl143@mail.ru>
2026-07-24 15:35:48 +0300

    fix: remove all unnecessary tables and relations in schema.sql

--- apps/backend/features/notes/schema.sql
+++ apps/backend/features/notes/schema.sql
@@ -1,20 +1,7 @@
 CREATE TABLE
     IF NOT EXISTS notes (
         id UUID PRIMARY KEY DEFAULT uuidv7 (),
-        user_id UUID DEFAULT,
+        user_id UUID DEFAULT uuidv7 (),
         title TEXT DEFAULT '',
         content TEXT DEFAULT '',
-    );
-
-CREATE TABLE
-    IF NOT EXISTS tags (
-        id UUID PRIMARY KEY DEFAULT uuidv7 (),
-        name TEXT UNIQUE NOT NULL,
-    );
-
-CREATE TABLE
-    IF NOT EXISTS note_tags (
-        note_id UUID REFERENCES notes (id) ON DELETE CASCADE NOT NULL,
-        tag_id UUID REFERENCES tags (id) ON DELETE CASCADE NOT NULL,
-        PRIMARY KEY (note_id, tag_id)
     );
